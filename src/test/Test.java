f6314bf39b2bd3852c4815f726231f5640cb6b5e
zhoushanglin <zshanglin@hotmail.com>
2015-11-26 16:21:50 +0800

    first type

---
+++ src/test/Test.java
@@ -0,0 +1,23 @@
+/* 
+ * Copyright (c) 2015, S.F. Express Inc. All rights reserved.
+ */
+package test;
+
+/**
+ * ÃèÊö£º
+ * 
+ * <pre>HISTORY
+ * ****************************************************************************
+ *  ID   DATE           PERSON          REASON
+ *  1    2015Äê11ÔÂ26ÈÕ      775456         Create
+ * ****************************************************************************
+ * </pre>
+ * @author 775456
+ * @since 1.0
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+
+}
